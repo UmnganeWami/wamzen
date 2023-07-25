328f4e132676e5d0d6d923f89cefc32349eca2df
Krismo <altac-lancing@hotmail.com>
2023-07-25 12:54:01 +1000

    sumn idk

--- src/wamzen/Bitmap.hx
+++ src/wamzen/Bitmap.hx
@@ -1,11 +1,12 @@
 package wamzen;
+import h2d.Object;
 import h2d.Bitmap as H2DBITMAP;
 import hxd.res.Image;
 import h2d.Tile;
 
 class Bitmap extends H2DBITMAP {
-    public function new(?sprite:Image){
-        super(null, null);
+    public function new(?sprite:Image, ?parent:Object){
+        super(null, parent);
         if(sprite != null) loadTile(sprite.toTile());
     }
 
